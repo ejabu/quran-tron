7c89f916cdaf131250f82da6263d63bed4b2563f
ejabu <muhammadfahreza@gmail.com>
2017-02-22 19:32:23 +0700

    Clean unused files and add shrinkwrap

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,21 +1,11 @@
 // @flow
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
-import counter from './counter';
-import posts from './posts';
-import posts2 from './posts2';
-import suggestions from './suggestions';
-import translations from './translations';
 import verseIndex from './verseIndex';
 import result from './result';
 
 const rootReducer = combineReducers({
-  counter,
   verseIndex,
-  posts,
-  posts2,
-  suggestions,
-  translations,
   result,
   routing
 });
