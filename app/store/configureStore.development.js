41226d0bdb5ff449b9197ea3d0e452393e393cec
ejabu <muhammadfahreza@gmail.com>
2017-05-11 18:48:33 +0700

    Clean

--- app/store/configureStore.development.js
+++ app/store/configureStore.development.js
@@ -5,10 +5,8 @@ import { routerMiddleware, push } from 'react-router-redux';
 import createLogger from 'redux-logger';
 import rootReducer from '../reducers';
 
-import * as counterActions from '../actions/counter';
 
 const actionCreators = {
-  ...counterActions,
   push,
 };
 
