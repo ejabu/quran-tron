b23655a70597de42213e98f1ec50626a2c97cf82
ejabu <muhammadfahreza@gmail.com>
2017-05-14 14:21:52 +0700

    Update package name and properties, update about page

--- app/main.development.js
+++ app/main.development.js
@@ -107,11 +107,11 @@ app.on('ready', async() => {
         }, {
           label: 'Paste',
           role: 'paste'
-        }, {
-          label: 'Inspect element',
-          click() {
-            mainWindow.inspectElement(x, y);
-          }
+        // }, {
+        //   label: 'Inspect element',
+        //   click() {
+        //     mainWindow.inspectElement(x, y);
+        //   }
         }
       ]).popup(mainWindow);
     });
@@ -321,43 +321,55 @@ app.on('ready', async() => {
           {
             label: 'About',
             click() {
-              // shell.openExternal('https://ejabu.github.io/quran-tron.html');
               if (aboutWindow == null){
                 aboutWindow = new BrowserWindow({
                                       skipTaskbar: true,
                                       frame: false,
                                       toolbar: false,
                                       width: 524,
-                                      height: 238,
+                                      height: 254,
                                       title: 'About'});
                 // Folder assets dibelakang app
                 aboutWindow.loadURL(`file://${__dirname}/../assets/about.html`);
+                aboutWindow.openDevTools();
 
                 aboutWindow.webContents.on('did-finish-load', () => {
                   aboutWindow.show();
                   aboutWindow.focus();
                 });
 
-                aboutWindow.on('blur', function() {
-                  aboutWindow.destroy()
-                  aboutWindow = null;
-                });
+                // aboutWindow.on('blur', function() {
+                //   aboutWindow.destroy()
+                //   aboutWindow = null;
+                // });
               }
 
             }
+          },
+          {
+            label: 'Check for Update',
+            click() {
+              shell.openExternal('https://aysaar.github.io/quran');
+            }
+          },
+          {
+            type: "separator"
+          },
+          {
+            label: 'Version 1.2.0',
+            enabled: false,
           }
         ] : [ //Ini kalau Production
           {
             label: 'About',
             click() {
-              // shell.openExternal('https://ejabu.github.io/quran-tron.html');
               if (aboutWindow == null){
                 aboutWindow = new BrowserWindow({
                                       skipTaskbar: true,
                                       frame: false,
                                       toolbar: false,
                                       width: 524,
-                                      height: 238,
+                                      height: 254,
                                       title: 'About'});
                 // Karena ini akan ke load di Resources
                 // Folder assets dibelakang resource dan dibelakang asar.resources
@@ -376,6 +388,19 @@ app.on('ready', async() => {
               }
 
             }
+          },
+          {
+            label: 'Check for Update',
+            click() {
+              shell.openExternal('https://aysaar.github.io/quran');
+            }
+          },
+          {
+            type: "separator"
+          },
+          {
+            label: 'Version 1.2.0',
+            enabled: false,
           }
         ]
       }
